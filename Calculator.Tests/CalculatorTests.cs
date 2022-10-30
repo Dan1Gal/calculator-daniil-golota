767c78aca06a5801be4da46865e10b893c41b137
Dan1Gal <dan-ok@bk.ru>
2022-10-30 20:41:16 +0300

    Calculator
    Calculator

---
+++ Calculator.Tests/CalculatorTests.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Xunit;
+
+namespace Calculator.Tests
+{
+    public class CalculatorTests
+    {
+        [Fact]
+        public void TestCalc()
+        {
+            //correct data
+            string expression = "8+2";
+            string expected = "10";
+            MyCalc calc = new MyCalc();
+            string actual = calc.Calc(expression);
+            Assert.Equal(expected, actual);
+
+            //not correct data
+            expression = "8/0";
+            expected = "NOT ÷ 0";
+            actual = calc.Calc(expression);
+            Assert.Equal(expected, actual);
+            expression = "999999999+1";
+            expected = "EXCEEDED";
+            actual = calc.Calc(expression);
+            Assert.Equal(expected, actual);
+            expression = "8d9";
+            expected = "error";
+            actual = calc.Calc(expression);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestMultiply()
+        {
+            string x = "2";
+            string y = "3";
+            string expected = "6";
+            MyCalc calc = new MyCalc();
+            string actual = calc.Multiply(x, y).ToString();
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestDivide()
+        {
+            string x = "9";
+            string y = "3";
+            string expected = "3";
+            MyCalc calc = new MyCalc();
+            string actual = calc.Divide(x, y).ToString();
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestSum()
+        {
+            string x = "2";
+            string y = "3";
+            string expected = "5";
+            MyCalc calc = new MyCalc();
+            string actual = calc.Sum(x, y).ToString();
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestMinus()
+        {
+            string x = "8";
+            string y = "3";
+            string expected = "5";
+            MyCalc calc = new MyCalc();
+            string actual = calc.Minus(x, y).ToString();
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestGetValues()
+        {
+            string expression = "8+2";
+            var expected = new { Item1 = "8", Item2 = "+", Item3 = "2" };
+            MyCalc calc = new MyCalc();
+            var actual = calc.GetValues(expression);
+            Assert.Equal(expected.Item1, actual.Item1);
+            Assert.Equal(expected.Item2, actual.Item2);
+            Assert.Equal(expected.Item3, actual.Item3);
+        }
+    }
+}
---
+++ Calculator/Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        string expression = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonOne_Click(object sender, EventArgs e)
+        {
+            setNum("1");
+        }
+
+        private void buttonTwo_Click(object sender, EventArgs e)
+        {
+            setNum("2");
+        }
+
+        private void buttonThree_Click(object sender, EventArgs e)
+        {
+            setNum("3");
+        }
+
+        private void buttonFour_Click(object sender, EventArgs e)
+        {
+            setNum("4");
+        }
+
+        private void buttonFive_Click(object sender, EventArgs e)
+        {
+            setNum("5");
+        }
+
+        private void buttonSix_Click(object sender, EventArgs e)
+        {
+            setNum("6");
+        }
+
+        private void buttonSeven_Click(object sender, EventArgs e)
+        {
+            setNum("7");
+        }
+
+        private void buttonEight_Click(object sender, EventArgs e)
+        {
+            setNum("8");
+        }
+
+        private void buttonNine_Click(object sender, EventArgs e)
+        {
+            setNum("9");
+        }
+
+        private void buttonZero_Click(object sender, EventArgs e)
+        {
+            setNum("0");
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            expression = richTextBox1.Text;
+            label1.Visible = false;
+        }
+
+        private void buttonPlusMinus_Click(object sender, EventArgs e)
+        {
+            label1.Visible = !label1.Visible;
+        }
+
+        
+        private void buttonMinus_Click(object sender, EventArgs e)
+        {
+            checkExpression();
+            setSign("-");
+        }
+        private void buttonDivide_Click(object sender, EventArgs e)
+        {
+            checkExpression();
+            setSign("/");
+        }
+        private void buttonSum_Click(object sender, EventArgs e)
+        {
+            checkExpression();
+            setSign("+");
+        }   
+        private void buttonMultiply_Click(object sender, EventArgs e)
+        {
+            checkExpression();
+            setSign("*");
+        }
+       
+
+        private void buttonEquals_Click(object sender, EventArgs e)
+        {
+            string result;
+            if (expression != "")
+            {
+                if (!int.TryParse(expression[expression.Length - 1].ToString(), out var number))
+                {
+                    expression = expression.Substring(0, expression.Length - 1);
+                }
+                if (label1.Visible)
+                {
+                    result = new MyCalc().Calc("-" + expression);
+                }
+                else
+                {
+                    result = new MyCalc().Calc(expression);
+                }
+                if (int.TryParse(result, out var num))
+                {
+                    checkNum(int.Parse(result));
+                }
+                else
+                {
+                    richTextBox1.Text = result;
+                }
+            }
+        }
+
+        private void checkExpression()
+        {
+            string result;
+            Regex regex = new Regex(@"[+, -]?\d+[+, \-, *, /]\d+");
+            if (regex.IsMatch(expression))
+            {
+                if (label1.Visible)
+                {
+                    result = new MyCalc().Calc("-" + expression);
+                }
+                else
+                {
+                    result = new MyCalc().Calc(expression);
+                }
+                if (int.TryParse(result, out var num))
+                {
+                    checkNum(int.Parse(result));
+                }
+                else
+                {
+                    richTextBox1.Text = result;
+                }
+            }
+        }
+
+        private void checkNum(int result)
+        {
+            if (result < 0)
+            {
+                richTextBox1.Text = (result * -1).ToString();
+                label1.Visible = true;
+                expression = "-" + richTextBox1.Text;
+            }
+            else
+            {
+                richTextBox1.Text = result.ToString();
+                label1.Visible = false;
+                expression = richTextBox1.Text;
+            }
+        }
+
+        private void setNum(string num)
+        {
+            if (richTextBox1.Text.Length <= 8)
+            {
+                if (expression != "" && int.TryParse(expression[expression.Length - 1].ToString(), out var number))
+                {
+                    richTextBox1.Text += num;
+                }
+                else
+                {
+                    richTextBox1.Text = num;
+                }
+                expression += num;
+            }
+        }
+
+        private void setSign(string sign)
+        {
+            if (expression == "" && sign == "-")
+            {
+                label1.Visible = !label1.Visible;
+            }
+            else
+            {
+                if (expression != "")
+                {
+                    if (int.TryParse(expression[expression.Length - 1].ToString(), out var number))
+                    {
+                        expression += sign;
+                    }
+                    else
+                    {
+                        expression = expression.Substring(0, expression.Length - 1).Insert(expression.Length - 1, sign);
+                    }
+                }
+            }
+        }
+    }
+}
